d657670459b3c7ab216e1e5b55dd1115c412c1c9
xgli <xgli0807@gmail.com>
2016-06-11 19:57:28 +0800

    only create project

---
+++ src/main/java/com/li/spark/EL/App.java
@@ -0,0 +1,13 @@
+package com.li.spark.EL;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
