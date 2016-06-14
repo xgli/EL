e22f0ee7f43d87cce55bf29d6527f73a157fbf58
xgli <xgli0807@gmail.com>
2016-06-14 14:22:29 +0800

    save segment text

--- src/main/java/edu/li/test/App.java
+++ src/main/java/edu/li/test/App.java
@@ -38,6 +38,7 @@ public class App {
 	 */
 	public static final String FILEINPUTDIR = "data" + File.separator + "xmlParse" + File.separator;
 	public static final String FILEOUTDIR = "data" + File.separator + "mention" + File.separator;
+	public static final String SEGMENTOUTDIR = "data" + File.separator + "segment" + File.separator;
 	
 	public static String getAnsjSegment(String text, String host, int port) throws IOException{
 	  	 StringReader sr = new StringReader(text);
@@ -46,15 +47,12 @@ public class App {
 		 StringWriter sw = new StringWriter(); // create client writer not to write a file
 		 BufferedWriter bw = new BufferedWriter(sw);
 
-//		 segServer.segClient.communicateWithsegServer("10.103.28.254", 4465 ,"utf-8",br,bw,false); 
 		 segServer.segClient.communicateWithsegServer(host, port, "utf-8",br,bw,false);
 		 br.close();
 		 bw.close();
 		 String[] lines = sw.toString().split("\n");
 		 StringBuilder sb = new StringBuilder();
 		 for(String line : lines){
-//			 System.out.println(line);
-//			 System.out.println("line:" + line.substring(1, line.length()-1));
 			 line = line.substring(1,line.length()-1);
 			 String[] tokens = line.split(", ");
 			 for(String token : tokens){
@@ -70,7 +68,6 @@ public class App {
 		 BufferedReader br = new BufferedReader(sr);
 		 StringWriter sw = new StringWriter();
 		 BufferedWriter bw = new BufferedWriter(sw);
-//		 NERClient.communicateWithNERServer("10.103.28.254", 2310, "UTF-8", br, bw, false); 
 		 NERClient.communicateWithNERServer(host, port, "UTF-8",br,bw,false);
 		 bw.close();
 		 br.close();
@@ -80,17 +77,24 @@ public class App {
 	public static void main(String[] args) throws IOException {
 		
 		// TODO Auto-generated method stub
-		 String text = IOUtils.slurpFile(FILEINPUTDIR + "CMN_NW_000020_20150604_F00100013.nw.xml");
-//		 String str = "美国总统发表演讲。杜甫是的诗人,石家庄位于黑龙江省。华盛顿位于美国。林光是一名伟大的球员,李白是诗人。清华大学的学生。北京大学生在一起";
-		 FileOutputStream fos = new FileOutputStream(FILEOUTDIR +"CMN_NW_000020_20150604_F00100013.nw.xml");
-		 OutputStreamWriter osw = new OutputStreamWriter(fos, "UTF-8");	 
+		 String fileName = "CMN_NW_000020_20150604_F00100013.nw.xml";
+		 String text = IOUtils.slurpFile(FILEINPUTDIR + fileName);	 
 		 String segText = getAnsjSegment(text,"10.103.28.254",4465);
 		 String[] lines = segText.split("\n");
 		 int start = 0; //mention location the first char.
 		 int end = 0;  //mention location the lastest char.	
+		 
+		 FileOutputStream segfos = new FileOutputStream(SEGMENTOUTDIR + fileName);
+		 OutputStreamWriter segosw = new OutputStreamWriter(segfos, "UTF-8");
+		 
+		 FileOutputStream nerfos = new FileOutputStream(FILEOUTDIR + fileName);
+		 OutputStreamWriter nerosw = new OutputStreamWriter(nerfos, "UTF-8");
 		 for(String line:lines){
 			 int bias = Integer.parseInt(line.split(" ")[0].trim());
-			 String ner = getNer(line.split(" ")[1],"10.103.28.254",2310);
+			 String segLine = line.split(" ")[1];
+			 segosw.write(segLine + "\n");
+			 segosw.flush();
+			 String ner = getNer(segLine,"10.103.28.254",2310);
 			 int len = 0;
 			 Pattern pattern = Pattern.compile("(<.*?>)(.*?)</.*?>");
 			 Matcher matcher = pattern.matcher(ner);
@@ -102,17 +106,18 @@ public class App {
 				 String type = matcher.group(1);
 				 String loc = start + "-" + end;
 				 System.out.print(mention + "\t");
-				 osw.write(mention + "\t");
+				 nerosw.write(mention + "\t");
 				 System.out.print(type + "\t");
-				 osw.write(type + "\t");
+				 nerosw.write(type + "\t");
 				 System.out.println(loc);
-				 osw.write(loc + "\n");
-				 osw.flush();
+				 nerosw.write(loc + "\n");
+				 nerosw.flush();
 			 }
 		 }
-		 osw.close();
-		 fos.close();	 	 	 
-
+		 nerosw.close();
+		 nerfos.close(); 
+		 segosw.close();
+		 segfos.close();
 	}
 
 }
