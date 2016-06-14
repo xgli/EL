811bd8e839a300da816b2451b886d853a1851318
xgli <xgli0807@gmail.com>
2016-06-14 11:23:47 +0800

    fix filepath

--- src/main/java/edu/li/test/App.java
+++ src/main/java/edu/li/test/App.java
@@ -6,6 +6,7 @@ package edu.li.test;
 import java.awt.SystemTray;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -13,6 +14,8 @@ import java.io.StringReader;
 import java.io.StringWriter;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import javax.sound.sampled.Port;
 
 import org.ejml.alg.dense.linsol.qr.LinearSolverQrHouseTran;
 
@@ -33,14 +36,18 @@ public class App {
 	 * @param args
 	 * @throws IOException 
 	 */
-	public static String getAnsjSegment(String text) throws IOException{
+	public static final String FILEINPUTDIR = "data" + File.separator + "xmlParse" + File.separator;
+	public static final String FILEOUTDIR = "data" + File.separator + "mention" + File.separator;
+	
+	public static String getAnsjSegment(String text, String host, int port) throws IOException{
 	  	 StringReader sr = new StringReader(text);
 		 BufferedReader br = new BufferedReader(sr);
 		 
 		 StringWriter sw = new StringWriter(); // create client writer not to write a file
 		 BufferedWriter bw = new BufferedWriter(sw);
 
-		segServer.segClient.communicateWithsegServer("10.103.28.254", 4465 ,"utf-8",br,bw,false); 
+//		 segServer.segClient.communicateWithsegServer("10.103.28.254", 4465 ,"utf-8",br,bw,false); 
+		 segServer.segClient.communicateWithsegServer(host, port, "utf-8",br,bw,false);
 		 br.close();
 		 bw.close();
 		 String[] lines = sw.toString().split("\n");
@@ -58,12 +65,13 @@ public class App {
 		 return sb.toString();		
 	}
 	
-	public static String getNer(String text) throws IOException{
+	public static String getNer(String text, String host, int port) throws IOException{
 	   	 StringReader sr = new StringReader(text); //输入切分好的text
 		 BufferedReader br = new BufferedReader(sr);
 		 StringWriter sw = new StringWriter();
 		 BufferedWriter bw = new BufferedWriter(sw);
-		 NERClient.communicateWithNERServer("10.103.28.254", 2310, "UTF-8", br, bw, false); 
+//		 NERClient.communicateWithNERServer("10.103.28.254", 2310, "UTF-8", br, bw, false); 
+		 NERClient.communicateWithNERServer(host, port, "UTF-8",br,bw,false);
 		 bw.close();
 		 br.close();
 		 String ner = sw.toString().replaceAll("\t", "").replaceAll("LOC", "GPE").replaceAll("PERSON", "PER");
@@ -72,17 +80,17 @@ public class App {
 	public static void main(String[] args) throws IOException {
 		
 		// TODO Auto-generated method stub
-		 String text = IOUtils.slurpFile("raw_num.txt");
+		 String text = IOUtils.slurpFile(FILEINPUTDIR + "CMN_NW_000020_20150604_F00100013.nw.xml");
 //		 String str = "美国总统发表演讲。杜甫是的诗人,石家庄位于黑龙江省。华盛顿位于美国。林光是一名伟大的球员,李白是诗人。清华大学的学生。北京大学生在一起";
-		 FileOutputStream fos = new FileOutputStream("mention.txt");
-		 OutputStreamWriter osw = new OutputStreamWriter(fos, "UTF-8");
-		 int start = 0; //mention location the first char.
-		 int end = 0;  //mention location the lastest char.		 
-		 String segText = getAnsjSegment(text);
+		 FileOutputStream fos = new FileOutputStream(FILEOUTDIR +"CMN_NW_000020_20150604_F00100013.nw.xml");
+		 OutputStreamWriter osw = new OutputStreamWriter(fos, "UTF-8");	 
+		 String segText = getAnsjSegment(text,"10.103.28.254",4465);
 		 String[] lines = segText.split("\n");
+		 int start = 0; //mention location the first char.
+		 int end = 0;  //mention location the lastest char.	
 		 for(String line:lines){
 			 int bias = Integer.parseInt(line.split(" ")[0].trim());
-			 String ner = getNer(line.split(" ")[1]);
+			 String ner = getNer(line.split(" ")[1],"10.103.28.254",2310);
 			 int len = 0;
 			 Pattern pattern = Pattern.compile("(<.*?>)(.*?)</.*?>");
 			 Matcher matcher = pattern.matcher(ner);
--- src/main/java/edu/li/xmlParse/xmlParse.java
+++ src/main/java/edu/li/xmlParse/xmlParse.java
@@ -9,6 +9,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
+import java.sql.SQLNonTransientConnectionException;
 import java.util.List;
 
 import org.dom4j.Document;
@@ -29,16 +30,22 @@ public class xmlParse {
 	 * @throws DocumentException 
 	 * @throws IOException 
 	 */
-	public static void Parse(String fileName) throws DocumentException, IOException{
-		
-		FileOutputStream fos = new FileOutputStream("raw_num.txt");
-		OutputStreamWriter osw = new OutputStreamWriter(fos, "UTF-8");
+	public  static final String FILEOUTDIR = "data" + File.separator + "xmlParse" + File.separator;
+	public static final String FILEINPUTDIR = "data" + File.separator + "raw" + File.separator;
+	
+	public static void Parse(String filePath) throws DocumentException, IOException{
+
+
 		SAXReader saxReader = new SAXReader();
-		File file = new File(fileName);
+		File file = new File(filePath);
 		Document document = saxReader.read(file);
 		Element LCTL_TEXT = document.getRootElement(); //LCTL_TEXT
 //		System.out.println(.getName());
-		Element DOC = (Element) LCTL_TEXT.elements().get(0);		
+		Element DOC = (Element) LCTL_TEXT.elements().get(0);
+		String fileOutPath = FILEOUTDIR + DOC.attributeValue("id");
+		FileOutputStream fos = new FileOutputStream(fileOutPath);
+		OutputStreamWriter osw = new OutputStreamWriter(fos, "UTF-8");
+		
 		Element TEXT = (Element) DOC.elements().get(0);
 		List<Element> SEGs = TEXT.elements();
 		for(Element SEG : SEGs){
@@ -58,8 +65,10 @@ public class xmlParse {
 	}
 	public static void main(String[] args) throws IOException, DocumentException {
 		// TODO Auto-generated method stub
-		String fileName = "raw.xml";
-		Parse(fileName);
+		String filePath = FILEINPUTDIR +"CMN_NW_000020_20150604_F00100013.nw.xml";//路径需要拼接，避免不同的平台使用。
+		Parse(filePath);
+		System.out.println(System.getProperty("user.dir"));
+
 
 	}
 
